291e85940863ede6b887313e5ae1943f45a3668f
eveehguo <eveehguo@gmail.com>
2024-01-20 19:21:27 -0500

    add BillboardSprite prefab

---
+++ Assets/Scripts/AlignToCamera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class AlignToCamera : MonoBehaviour
+{
+    private void Start()
+    {
+        var cam = Camera.main;
+        if (cam)
+        {
+            transform.rotation = cam.transform.rotation;
+        }
+    }
+}
